81c04494df266215ebea47a88a8f9a049c45261a
hisey <2929736912@qq.com>
2018-10-16 10:16:59 +0800

    Update index.js

--- src/filters/index.js
+++ src/filters/index.js
@@ -17,6 +17,21 @@ export function timeAgo(time) {
     return pluralize(~~(between / 86400), ' day')
   }
 }
+//时间戳转换为时间
+export  function dataFormat(val) {
+      if (val == "" || val == 0) return "";
+      let timeMinSeconds = parseInt(val);
+      function fix(num) {
+        return num >= 10 ? num : "0" + num;
+      }
+      var date = new Date(timeMinSeconds);
+      var year = date.getFullYear();
+      var month = fix(date.getMonth() + 1);
+      var day = fix(date.getDate());
+      var hour = fix(date.getHours());
+      var minute = fix(date.getMinutes());
+      return year + "-" + month + "-" + day + " " + hour + ":" + minute;
+ }
 
 /* 数字 格式化*/
 // export function numberFormatter(num, digits) {
